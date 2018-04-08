4dad61df24a6aa3672b8c0814465431912a5aef7
mwaz <amby.mwaz@gmail.com>
2018-04-08 19:38:34 +0300

    [ch] add controllers and routes to app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  */
 const express = require("express");
 const bodyParser = require("body-parser");
-const routes = require("./app/routes");
+const routes = require("./app/routes/recipes.routes");
 const dbConfig = require("./config/database.config");
 const moongoose = require("mongoose");
 
